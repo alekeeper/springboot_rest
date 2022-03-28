4c6207ef06a0fbb8379acd141b4b20e060ff7d61
Oleg Sergienko <keep233@gmail.com>
2022-03-29 00:56:32 +0300

    springboot_rest project

---
+++ src/main/java/com/example/springboot_rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.springboot_rest.service;
+
+import com.example.springboot_rest.dao.EmployeeDAO;
+import com.example.springboot_rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return (Employee) employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+
+    }
+
+
+}
